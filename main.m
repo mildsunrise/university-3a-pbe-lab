49eaafa73b45df85dd1b8ba6051bdcf4b100790e
Alba Mendez <alba.mendez@basetis.com>
2019-01-16 14:23:17 +0100

    App chooser! Write main script and path handling

---
+++ main.m
@@ -0,0 +1,14 @@
+% Add subdirectories to path
+root = fileparts(mfilename('fullpath'));
+addpath([root filesep 'assets']);
+addpath([root filesep 'assets' filesep 'open-iconic']);
+addpath([root filesep 'src' filesep 'adc']);
+addpath([root filesep 'src' filesep 'analyzer']);
+addpath([root filesep 'src' filesep 'equalizer']);
+addpath([root filesep 'src' filesep 'simulator']);
+addpath([root filesep 'src' filesep 'util']);
+addpath([root filesep 'src' filesep 'util' filesep 'dsp']);
+addpath([root filesep 'src' filesep 'util' filesep 'misc']);
+
+% Run chooser app
+chooser;
